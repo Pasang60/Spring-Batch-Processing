9fbe5174ccf75f97021ae794d748fe35c128b1de
Pasang60 <pasanggelbu@gmail.com>
2025-10-01 22:46:54 +0545

    continuing the set up with database schema design and folder structure

--- batchProcessing/src/main/java/com/batch/batchProcessing/admin/entity/Users.java
+++ batchProcessing/src/main/java/com/batch/batchProcessing/admin/entity/Users.java
@@ -2,15 +2,18 @@ package com.batch.batchProcessing.admin.entity;
 
 /*
  * @author Pasang Gelbu Sherpa *
+ * @created 6/6/2024 at 11:28 PM
  */
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.batch.batchProcessing.role.entity.Roles;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -25,7 +28,17 @@ public class Users {
     private String email;
     private String address;
     private String phone;
+    private LocalDate dateOfBirth;
+    private Long age;
     private String profilePic;
     private String password;
     private boolean isVerified;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_entity_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id")
+    )
+    private List<Roles> roles = new ArrayList<>();
 }
